a22b456416373234d22181aacb2c6b95242cd115
QuXiaoMing <xm726@qq.com>
2018-11-13 19:03:34 +0800

    第四章：使用Druid作为SpringBoot项目数据源（添加监控）

---
+++ lesson4/src/main/java/com/example/lesson4/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.example.lesson4.controller;
+
+import com.example.lesson4.entity.UserEntity;
+import com.example.lesson4.jpa.UserJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @Autowired
+    private UserJPA userJPA;
+
+    /**
+     * 查询用户列表方法
+     * @return
+     */
+    @RequestMapping(value ="/list" ,method = RequestMethod.GET)
+    public List<UserEntity> list(){
+        return userJPA.findAll();
+    }
+
+    /**
+     * 添加、更新用户方法
+     * @param entity
+     * @return
+     */
+    @RequestMapping(value = "/save",method = RequestMethod.GET)
+    public UserEntity save(HttpServletRequest request)
+    {
+        String name = request.getParameter("name");
+        UserEntity entity = new UserEntity();
+        entity.setName(name);
+        return userJPA.save(entity);
+    }
+
+    /**
+     * 删除用户方法
+     * @param id 用户编号
+     * @return
+     */
+    @RequestMapping(value = "/delete",method = RequestMethod.GET)
+    public List<UserEntity> delete(Long id)
+    {
+        userJPA.delete(id);
+        return userJPA.findAll();
+    }
+}
